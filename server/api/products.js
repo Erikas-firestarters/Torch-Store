bc02e2a6547e35ffe8e4b87ba9a637bd76a2b097
Josh Remaley <joshremaley@gmail.com>
2018-04-17 16:33:52 -0500

    finished routes

---
+++ server/api/products.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+const { Product, Review } = require('../db/models')
+module.exports = router
+
+router.get('/', (req, res, next) => {
+  Product.findAll()
+    .then(products => res.json(products))
+    .catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+  Product.findById(req.params.id)
+    .then(product => res.json(product))
+    .catch(next)
+})
+
+router.get('/:id/review', (req, res, next) => {
+  Review.findAll({
+    where: {
+      productId: req.params.id
+    }
+  })
+    .then(reviews => res.json(reviews))
+    .catch(next)
+})
+
+router.post('/:id/review', (req, res, next) => {
+  Review.create(req.body)
+    .then(review => res.json(review))
+    .catch(next)
+})
+
+router.post('/:id/review', (req, res, next) => {
+  Review.create(req.body)
+    .then(review => res.json(review))
+    .catch(next)
+})
