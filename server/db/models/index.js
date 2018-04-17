102ec2481bb90947ed70b0c2bf49f5ab18803c65
thesamkogan <thesamkogan@gmail.com>
2018-04-17 09:47:30 -0500

    added associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,7 @@
 const User = require('./user')
 const CartItem = require('./cartItem')
 const Cart = require('./cart')
+const Product = require('./product')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -8,6 +9,13 @@ const Cart = require('./cart')
  *
  *    BlogPost.belongsTo(User)
  */
+Cart.belongsTo(User)
+CartItem.belongsTo(Cart)
+Product.belongsTo(CartItem)
+
+User.hasOne(Cart)
+Cart.hasMany(CartItem)
+CartItem.hasOne(Product)
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
@@ -19,4 +27,5 @@ module.exports = {
   User,
   Cart,
   CartItem,
+  Product,
 }
