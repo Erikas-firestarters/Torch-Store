d003900d0a4426806c8d13524ff23f7cb52d78f5
Josh Remaley <joshremaley@gmail.com>
2018-04-17 09:21:15 -0500

    address model ready for review

---
+++ server/db/models/address.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Address = db.define('address', {
+    fullName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    addressLine1: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    addressLine2: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    city: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    state: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    zipcode: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+})
+
+module.exports = Address
