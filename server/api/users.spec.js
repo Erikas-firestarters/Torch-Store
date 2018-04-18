6e9d2240a73895ab9304f4dbd19b4422279c9fee
Caitlin Trussell <caitlintrussell@gmail.com>
2018-04-18 11:13:58 -0500

    completed get tests

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -5,29 +5,79 @@ const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
+const Cart = db.model('cart');
+const CartItem = db.model('cartItem');
+const Product = db.model('product');
 
-describe('User routes', () => {
+describe.only('User routes', () => {
+  const user = {
+    email: 'cody@puppybook.com',
+    password: 'bones',
+    firstName: 'Cody',
+    lastName: 'Bones'
+  };
+  const cart = { quantity: 1 };
+  const cartItem = { quantity: 1 };
+  const product = {
+    name: 'El Flamo',
+    description: 'hotter than hell, (batteries not included)',
+    price: 40.44,
+    inventory: 20
+  }
   beforeEach(() => {
     return db.sync({force: true})
   })
 
   describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
-
     beforeEach(() => {
-      return User.create({
-        email: codysEmail
-      })
+      return User.create(user)
     })
 
     it('GET /api/users', () => {
       return request(app)
         .get('/api/users')
         .expect(200)
-        .then(res => {
-          expect(res.body).to.be.an('array')
-          expect(res.body[0].email).to.be.equal(codysEmail)
+        .then(({body}) => {
+          expect(body).to.be.an('array')
+          expect(body[0].email).to.be.equal('cody@puppybook.com')
+      })
+    })
+  })
+  describe('/api/users/:id', () => {
+    beforeEach(async () => {
+      await User.create(user)
+    })
+
+    it('GET /api/users/1', () => {
+      return request(app)
+        .get('/api/users/1')
+        .expect(200)
+        .then(({body}) => {
+          expect(body).to.be.an('object')
+          expect(body.email).to.be.equal('cody@puppybook.com')
+      })
+    })
+  })
+  describe('/api/users/:id/cart', () => {
+
+    beforeEach( async () => {
+      const newUser = await User.create(user);
+      const newProduct = await Product.create(product);
+      const newCart = await Cart.create(cart);
+      const newCartItem = await CartItem.create(cartItem);
+      newProduct.setCartItem(newCartItem);
+      newCartItem.setCart(newCart);
+      newCart.setUser(newUser);
+    })
+
+    it('GET /api/users/:id/cart', () => {
+      return request(app)
+        .get('/api/users/1/cart')
+        .expect(200)
+        .then( ({body}) => {
+          expect(body).to.be.an('object');
+          expect(body.cartItems).to.be.an('array');
         })
     })
-  }) // end describe('/api/users')
+  })
 }) // end describe('User routes')
