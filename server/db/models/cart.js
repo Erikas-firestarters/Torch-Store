607d0c1ef1b641f343962e7d8f2c662375ef64c6
thesamkogan <thesamkogan@gmail.com>
2018-04-18 17:18:34 -0500

    final unfinished yo

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -3,16 +3,23 @@ const db = require('../db');
 const CartItem = db.model('cartItem');
 
 const Cart = db.define('cart', {
-  quatity: {
-    type: Sequelize.VIRTUAL,
-    get: function () {
-      return CartItem.findAll({
-        where: {
-          cartId: this.id
-        }
-      }).then(items => items.length)
-     }
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1
   }
+  // quantity: {
+  //   type: Sequelize.VIRTUAL,
+  //   get: function () {
+  //     return CartItem.findAll({
+  //       where: {
+  //         cartId: this.id
+  //       }
+  //     }).then(items => {
+  //       console.log('setting quantity for cart')
+  //       return items.length
+  //     })
+  //    }
+  // }
 });
 
 module.exports = Cart;
