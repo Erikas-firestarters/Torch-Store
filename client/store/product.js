7c9159d779996f3be40eba721b34aa4dba335f8f
Caitlin Trussell <caitlintrussell@gmail.com>
2018-04-19 10:19:43 -0500

    product reducers complete for now

--- client/store/product.js
+++ client/store/product.js
@@ -8,19 +8,31 @@ const ADD_PRODUCT = 'ADD_PRODUCT';
 
 const selectedProduct = {};
 
-const getProducts = (products) => ({type: GET_PRODUCTS, products});
-
 const getProduct = (product) => ({type: GET_PRODUCT, product});
 const removeProduct = () => ({type: REMOVE_PRODUCT});
 const updateProduct = (product) => ({type: UPDATE_PRODUCT, product});
 const addProduct = (product) => ({type: ADD_PRODUCT, product});
 
+export const fetchProduct = (id) =>
+  (dispatch) =>
+    axios.get(`/products/${id}`)
+    .then( res => dispatch(getProduct(res.data)))
+    .catch( err => console.error(err));
 
+/* ____TODO_____
+  SET UP AXIOS REQUESTS FOR ADMIN PUT/POST/DELETE */
 
-// export default function (state = products, action) {
-//  switch (action.type) {
-//    case GET_PRODUCT: {
-
-//    }
-//  }
-// }
+export default function (state = selectedProduct, action) {
+  switch (action.type) {
+    case GET_PRODUCT:
+      return action.product;
+    case REMOVE_PRODUCT:
+      return action;
+    case UPDATE_PRODUCT:
+      return action.product;
+    case ADD_PRODUCT:
+      return action.product;
+    default:
+      return state;
+ }
+}
