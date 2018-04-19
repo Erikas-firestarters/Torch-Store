8752a61489d9862c58448100398ba7fb8e8f5c86
Josh Remaley <joshremaley@gmail.com>
2018-04-18 20:53:00 -0500

    had to replace BOILERPLATE

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>FIRE SALE AT THE TERRIFIC TORCH TAVERN</h1>
     <nav>
       {isLoggedIn ? (
         <div>
