ebbc0a07d4a91b082073a3d29533b16f69cea04e
Daniel Simandl <dsimandl419@gmail.com>
2018-04-18 21:39:53 -0500

    working on navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,56 +1,76 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { logout } from '../store';
+import { Input, Menu, Button, Grid } from 'semantic-ui-react';
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
+    {isLoggedIn ? (
+      <div>
+        {/* The navbar will show these links after you log in */}
+        <NavLink to="/home">
+          <Menu.Item name="home" />
+        </NavLink>
+        <a href="#" onClick={handleClick}>
+          Logout
+        </a>
+      </div>
+    ) : (
+      <div>
+        {/* The navbar will show these links before you log in */}
+        <Grid columns="equal">
+          <Menu top="true">
+            <Grid.Column width={8}>
+              <Menu.Menu position="left" fluid={'true'}>
+                <Menu.Item>
+                  <Input icon="search" placeholder="Search..." />
+                </Menu.Item>
+              </Menu.Menu>
+            </Grid.Column>
+
+            <Grid.Column width={1}>
+              <Menu.Menu position="right">
+                <NavLink to="/login">
+                  <Menu.Item name="login" />
+                </NavLink>
+                <NavLink to="/signup">
+                  <Menu.Item name="signup" />
+                </NavLink>
+              </Menu.Menu>
+            </Grid.Column>
+          </Menu>
+        </Grid>
+      </div>
+    )}
     <hr />
   </div>
-)
+);
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id
-  }
-}
+    isLoggedIn: !!state.user.id,
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
     handleClick() {
-      dispatch(logout())
-    }
-  }
-}
+      dispatch(logout());
+    },
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
 
 /**
  * PROP TYPES
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+  isLoggedIn: PropTypes.bool.isRequired,
+};
