508c801f14c9ff883763c2773b7fadca7fbb44e7
Daniel Simandl <dsimandl419@gmail.com>
2018-04-25 05:39:13 -0500

    cart quantity showing on navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,73 +2,83 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { logout } from '../store';
 import { Header, Menu, Label, Icon } from 'semantic-ui-react';
-=======
-import { logout, emptyCart } from '../store';
-=======
 import { logout, emptyReduxCart } from '../store';
->>>>>>> b5dae02f3a6a5beea4217f062686bb202caef1a7
-import { Header, Menu, Label } from 'semantic-ui-react';
->>>>>>> b9384af9b393a916924ba43da024a11599c13bde
 import SearchBar from './search';
 const resultRenderer = ({ name }) => <Label content={name} />;
-const Navbar = ({ handleClick, isLoggedIn, isAdmin, cart }) => (
-  <div>
-    {isLoggedIn ? (
-      <div>
-        {/* The navbar will show these links after you log in */}
-        <Menu>
-          <Menu.Item>
-            <img src="https://vignette.wikia.nocookie.net/elderscrolls/images/9/93/TESV_Torch.png/revision/latest?cb=20120619180715" />
-          </Menu.Item>
-          <Menu.Item as={NavLink} to="/products" name="Shop Torches" link />
-          <Menu.Item>
-            <SearchBar resultRenderer={resultRenderer} />
-          </Menu.Item>
-          <Menu.Menu position="right">
-            {isAdmin && (
-              <Menu.Item as={NavLink} to="/admin" name="admin" link />
-            )}
-            <Menu.Item as={NavLink} to="/home" name="home" link />
-            <Menu.Item name="logout" onClick={() => handleClick()} link />
-            <Menu.Item as={NavLink} to="/cart" link>
-              <Icon name="cart" />
-              <Label color="teal" floating>
-                {cart.length}
-              </Label>
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
-      </div>
-    ) : (
-      <div>
-        {/* The navbar will show these links before you log in */}
-        <Menu>
-          <Menu.Item>
-            <img src="https://vignette.wikia.nocookie.net/elderscrolls/images/9/93/TESV_Torch.png/revision/latest?cb=20120619180715" />
-          </Menu.Item>
-          <Menu.Item as={NavLink} to="/products" name="Shop Torches" link />
-          <Menu.Item>
-            <SearchBar />
-          </Menu.Item>
-          <Menu.Menu position="right">
-            <Menu.Item as={NavLink} to="/login" name="login" link />
-            <Menu.Item as={NavLink} to="/signup" name="signup" link />
+const Navbar = ({ handleClick, isLoggedIn, isAdmin, cart }) => {
+  const quantity = cart.reduce((acc, curr) => acc + curr.quantity, 0);
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div>
+          {/* The navbar will show these links after you log in */}
+          <Menu>
             <Menu.Item>
-              <Label color="teal">
-                {cart.length}
-              </Label>
+              <img src="https://vignette.wikia.nocookie.net/elderscrolls/images/9/93/TESV_Torch.png/revision/latest?cb=20120619180715" />
             </Menu.Item>
-            <Menu.Item as={NavLink} to="/cart" link icon="cart" />
-          </Menu.Menu>
-        </Menu>
-      </div>
-    )}
-  </div>
-);
+            <Menu.Item as={NavLink} to="/products" name="Shop Torches" link />
+            <Menu.Item>
+              <SearchBar resultRenderer={resultRenderer} />
+            </Menu.Item>
+            <Menu.Menu position="right">
+              {isAdmin && (
+                <Menu.Item as={NavLink} to="/admin" name="admin" link />
+              )}
+              <Menu.Item as={NavLink} to="/home" name="home" link />
+              <Menu.Item name="logout" onClick={() => handleClick()} link />
+              <Menu.Item as={NavLink} to="/cart" link>
+                <Icon name="cart" size="large">
+                  {quantity ? (
+                    <Label
+                      circular
+                      size="large"
+                      color="teal"
+                      attached="top right"
+                    >
+                      {quantity}
+                    </Label>
+                  ) : null}
+                </Icon>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </div>
+      ) : (
+        <div>
+          {/* The navbar will show these links before you log in */}
+          <Menu>
+            <Menu.Item>
+              <img src="https://vignette.wikia.nocookie.net/elderscrolls/images/9/93/TESV_Torch.png/revision/latest?cb=20120619180715" />
+            </Menu.Item>
+            <Menu.Item as={NavLink} to="/products" name="Shop Torches" link />
+            <Menu.Item>
+              <SearchBar />
+            </Menu.Item>
+            <Menu.Menu position="right">
+              <Menu.Item as={NavLink} to="/login" name="login" link />
+              <Menu.Item as={NavLink} to="/signup" name="signup" link />
+              <Menu.Item as={NavLink} to="/cart" link>
+                <Icon name="cart" size="large">
+                  {quantity ? (
+                    <Label
+                      circular
+                      size="large"
+                      color="teal"
+                      attached="top right"
+                    >
+                      {quantity}
+                    </Label>
+                  ) : null}
+                </Icon>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </div>
+      )}
+    </div>
+  );
+};
 
 /**
  * CONTAINER
