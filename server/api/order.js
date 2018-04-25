492ea77f41f0236b1bc9efa381dda6677690ee02
Josh Remaley <joshremaley@gmail.com>
2018-04-24 19:26:13 -0500

    work in progress

--- server/api/order.js
+++ server/api/order.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { User, Order, OrderItem, Address } = require('../db/models');
 const HttpError = require('../utils/HttpError');
+const transporter = require('../mailer/');
 module.exports = router;
 
 router.post('/', async (req, res, next) => {
-
   req.body.billing.zipcode = Number(req.body.billing.zipcode);
   req.body.shipping.zipcode = Number(req.body.shipping.zipcode);
 
@@ -29,6 +29,26 @@ router.post('/', async (req, res, next) => {
       thisOrder.setOrder(order).catch(HttpError);
     });
   });
+  let message = {
+    to: 'Josh Remaley <joshremaley@gmail.com>',
 
-  Promise.all(orderItems).then(res.sendStatus(201));
+    // Subject of the message
+    subject: 'Nodemailer is unicode friendly ✔',
+
+    // plaintext body
+    text: 'Hello to myself!',
+  };
+  Promise.all(orderItems).then(() => {
+    res.sendStatus(201);
+    transporter.sendMail(message, (error, info) => {
+      if (error) {
+        console.log('Error occurred');
+        console.log(error.message);
+        return process.exit(1);
+      }
+
+      console.log('Message sent successfully!');
+      transporter.close();
+    });
+  });
 });
--- server/mailer/index.js
+++ server/mailer/index.js
@@ -1,143 +1,110 @@
-// import nodemailer from 'nodemailer'
-
-// import config from './config'
-
-const user = 'firestartersgroupemail@gmail.com'
-const refreshToken = ''
-const accessToken = ''
-const clientId = '686570587105-kovcpba0ekivdu8o0vcpm1u0fq5kh0e6.apps.googleusercontent.com'
-const clientSecret = '2oX5I5V-t12hWkk8Ww6cGLzx'
-
-// const transporter = nodemailer.createTransport({
-//   service: 'Gmail',
-//   auth: {
-//     type: 'OAuth2',
-//     ...config
-//   }
-// });
-
-// const send = ({ email, name, text }) => {
-//   const from = name && email ? `${name} <${email}>` : `${name || email}`
-//   const message = {
-//     from,
-//     to: 'react.nodemailer@gmail.com',
-//     subject: `New message from ${from} at creating-contact-forms-with-nodemailer-and-react`,
-//     text,
-//     replyTo: from
-//   };
-
-//   return new Promise((resolve, reject) => {
-//     transporter.sendMail(message, (error, info) =>
-//       error ? reject(error) : resolve(info)
-//     )
-//   })
-// }
-
-// export default send
-
-/* eslint no-console: 0 */
-
-/* eslint no-console: 0 */
-
 'use strict';
+process.env.EMAIL_USER = 'firestartersgroupemail@gmail.com';
+process.env.EMAIL_REFRESH_TOKEN = '';
+process.env.ACCESS_TOKEN = '';
+process.env.CLIENT_ID =
+  '686570587105-kovcpba0ekivdu8o0vcpm1u0fq5kh0e6.apps.googleusercontent.com';
+process.env.CLIENT_SECRET = '2oX5I5V-t12hWkk8Ww6cGLzx';
 
 const nodemailer = require('nodemailer');
 
 // Generate SMTP service account from ethereal.email
 nodemailer.createTestAccount((err, account) => {
-    if (err) {
-        console.error('Failed to create a testing account');
-        console.error(err);
-        return process.exit(1);
+  if (err) {
+    console.error('Failed to create a testing account');
+    console.error(err);
+    return process.exit(1);
+  }
+
+  console.log('Credentials obtained, sending message...');
+
+  // NB! Store the account object values somewhere if you want
+  // to re-use the same account for future mail deliveries
+
+  // Create a SMTP transporter object
+  let transporter = nodemailer.createTransport(
+    {
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      logger: false,
+      debug: false, // include SMTP traffic in the logs
+    },
+    {
+      // default message fields
+
+      // sender info
+      from: 'FS <firestartersgroupemail@gmail.com>',
+      headers: {
+        'X-Laziness-level': 1000, // just an example header, no need to use this
+      },
     }
+  );
 
-    console.log('Credentials obtained, sending message...');
+  // Message object
+  let message = {
+    // Comma separated list of recipients
+    to: 'Daniel Simandl <dsimandl419@gmail.com>',
 
-    // NB! Store the account object values somewhere if you want
-    // to re-use the same account for future mail deliveries
+    // Subject of the message
+    subject: 'Nodemailer is unicode friendly ✔',
 
-    // Create a SMTP transporter object
-    let transporter = nodemailer.createTransport(
-        {
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'firestartersgroupemail@gmail.com',
-                pass: '666Password'
-            },
-            logger: false,
-            debug: false // include SMTP traffic in the logs
-        },
-        {
-            // default message fields
+    // plaintext body
+    text: 'Hello to myself!',
 
-            // sender info
-            from: 'FS <firestartersgroupemail@gmail.com>',
-            headers: {
-                'X-Laziness-level': 1000 // just an example header, no need to use this
-            }
-        }
-    );
+    // HTML body
+    html:
+      '<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>' +
+      '<p>Here\'s a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>',
 
-    // Message object
-    let message = {
-        // Comma separated list of recipients
-        to: 'Daniel Simandl <dsimandl419@gmail.com>',
+    // An array of attachments
+    attachments: [
+      // String attachment
+      {
+        filename: 'notes.txt',
+        content: 'Some notes about this e-mail',
+        contentType: 'text/plain', // optional, would be detected from the filename
+      },
 
-        // Subject of the message
-        subject: 'Nodemailer is unicode friendly ✔',
+      // Binary Buffer attachment
+      {
+        filename: 'image.png',
+        content: Buffer.from(
+          'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
+            '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
+            'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC',
+          'base64'
+        ),
 
-        // plaintext body
-        text: 'Hello to myself!',
+        cid: 'note@example.com', // should be as unique as possible
+      },
 
-        // HTML body
-        html:
-            '<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>' +
-            '<p>Here\'s a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>',
+      // File Stream attachment
+      // {
+      //     filename: 'nyan cat ✔.gif',
+      //     path: __dirname + '/assets/nyan.gif',
+      //     cid: 'nyan@example.com' // should be as unique as possible
+      // }
+    ],
+  };
 
-        // An array of attachments
-        attachments: [
-            // String attachment
-            {
-                filename: 'notes.txt',
-                content: 'Some notes about this e-mail',
-                contentType: 'text/plain' // optional, would be detected from the filename
-            },
+//   transporter.sendMail(message, (error, info) => {
+//     if (error) {
+//       console.log('Error occurred');
+//       console.log(error.message);
+//       return process.exit(1);
+//     }
 
-            // Binary Buffer attachment
-            {
-                filename: 'image.png',
-                content: Buffer.from(
-                    'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
-                        '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
-                        'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC',
-                    'base64'
-                ),
+//     console.log('Message sent successfully!');
+//     console.log(nodemailer.getTestMessageUrl(info));
 
-                cid: 'note@example.com' // should be as unique as possible
-            },
+//     // only needed when using pooled connections
+//     transporter.close();
+//   });
 
-            // File Stream attachment
-            // {
-            //     filename: 'nyan cat ✔.gif',
-            //     path: __dirname + '/assets/nyan.gif',
-            //     cid: 'nyan@example.com' // should be as unique as possible
-            // }
-        ]
-    };
-
-    transporter.sendMail(message, (error, info) => {
-        if (error) {
-            console.log('Error occurred');
-            console.log(error.message);
-            return process.exit(1);
-        }
-
-        console.log('Message sent successfully!');
-        console.log(nodemailer.getTestMessageUrl(info));
-
-        // only needed when using pooled connections
-        transporter.close();
-    });
+  module.exports = transporter;
 });
